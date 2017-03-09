045b79e1cef33027a2b5f665ad354e9c05671875
jpnsingh <jpnsingh86@gmail.com>
2017-03-09 23:09:32 +0530

    Using browserify along with tsify;  - bundling the ts into js by browserifying in order to be used by the browsers;

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,32 @@
 (function () {
     'use strict';
 
-    var gulp = require("gulp");
-    var ts = require("gulp-typescript");
-    var tsProject = ts.createProject("tsconfig.json");
+    var gulp = require('gulp');
+    var ts = require('gulp-typescript');
+    var tsProject = ts.createProject('tsconfig.json');
+    var browserify = require('browserify');
+    var source = require('vinyl-source-stream');
+    var tsify = require('tsify');
+    var paths = {
+        pages: ['app/src/*.html']
+    };
 
-    gulp.task("default", function () {
-        return tsProject.src()
-            .pipe(tsProject())
-            .js.pipe(gulp.dest("app/dist"));
+    gulp.task('copy-html', function () {
+        return gulp.src(paths.pages)
+            .pipe(gulp.dest('app/dist'));
+    });
+
+    gulp.task('default', ['copy-html'], function () {
+        return browserify({
+            basedir: './app',
+            debug: true,
+            entries: ['src/main.ts'],
+            cache: {},
+            packageCache: {}
+        })
+            .plugin(tsify)
+            .bundle()
+            .pipe(source('bundle.js'))
+            .pipe(gulp.dest('app/dist'));
     });
 })();
